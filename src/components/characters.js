141acb08c097244524222673b670c8867956da87
Andres Silva <a.silvag@uniandes.edu.co>
2021-05-11 23:56:06 -0500

    se fini

--- src/components/characters.js
+++ src/components/characters.js
@@ -40,7 +40,7 @@ const Characters = () => {
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey={c.id}>
-                                <Character info={c} key={c.id}/>
+                                <Detail info={c} key={c.id}/>
                             </Accordion.Collapse>
                         </Card>                    
                     ))}
